6c5e3b8c93102cc8f89e684a9be217dca119ac32
BUPTSSE-Zero <GGGZ-1101-28@Live.cn>
2015-11-03 21:29:47 +0800

    Add the feature of ADB startup and installing apk.

--- src/adb-tools/ADBTools.cpp
+++ src/adb-tools/ADBTools.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <glib.h>
+#include <unistd.h>
 #define MAX_SIZE 1024
 
 const char* ADBTools::ADB_PATH  = "./AndroidTools/adb";
@@ -11,6 +11,7 @@ const char* ADBTools::ADB_PATH  = "./AndroidTools/adb";
 ADBTools::ADBTools()
 {
     is_running = false;
+    task_thread = NULL;
 }
 
 void ADBTools::exec_adb_service_startup(ADBTools* data)
@@ -18,18 +19,23 @@ void ADBTools::exec_adb_service_startup(ADBTools* data)
     char startup_command[MAX_SIZE];
     sprintf(startup_command, "%s %s", ADB_PATH, "start-server");
     if(system(startup_command) == 0)
-    {
         data->is_running = true;
-        return ADB_START_SUCCESSFULLY;
-    }
     data->is_running = false;
+    g_cond_signal(&data->task_cond);
 }
 
 ADBTools::ADBStartError ADBTools::start_adb_service()
 {
-    GThread adb_start_thread = g_thread_create(exec_adb_service_startup, this, TRUE, NULL);
-    g_cond_wait_until()
-
+    task_thread = g_thread_new("ADBStartupThread", (GThreadFunc)exec_adb_service_startup, this);
+    g_mutex_init(&task_mutex);
+    g_cond_init(&task_cond);
+    gboolean ret = g_cond_wait_until(&task_cond, &task_mutex, g_get_monotonic_time() + STARTUP_TIME_OUT * G_TIME_SPAN_SECOND);
+    g_thread_unref(task_thread);
+    task_thread = NULL;
+    if(!ret)
+        return ADB_START_TIMEOUT;
+    else if(is_running)
+        return ADB_START_SUCCESSFULLY;
     int test_socket = socket(AF_INET, SOCK_STREAM, 0);
     if(test_socket < 0)
         return ADB_START_UNKNOWN_ERROR;
@@ -41,3 +47,16 @@ ADBTools::ADBStartError ADBTools::start_adb_service()
         return ADB_START_PORT_UNAVAILABLE;
     return ADB_START_UNKNOWN_ERROR;
 }
+
+bool ADBTools::install_apk(const char* apk_file_path)
+{
+    if(!is_running)
+        return false;
+    char install_command[MAX_SIZE];
+    sprintf(install_command, "%s %s %s", ADB_PATH, "install", apk_file_path);
+    if(system(install_command) == 0)
+        return true;
+    return false;
+}
+
+
--- src/adb-tools/ADBTools.h
+++ src/adb-tools/ADBTools.h
@@ -6,8 +6,13 @@
 class ADBTools
 {
     private:
+        static const int STARTUP_TIME_OUT = 15;
+
         bool is_running;
         static void exec_adb_service_startup(ADBTools* data);
+        GThread* task_thread;
+        GCond task_cond;
+        GMutex task_mutex;
     public:
         static const char* ADB_PATH;
         enum ADBStartError{
@@ -19,10 +24,13 @@ class ADBTools
 
         ADBTools();
         ADBStartError start_adb_service();
+
         bool is_run()
         {
             return is_running;
         }
+
+        bool install_apk(const char* apk_file_path);
 };
 
 #endif // ADBTOOLS_H
